f81bd12feca2cc52eed7f8a3df49223005a98617
Yusuf Can Korkmaz <korkmasyusufcan@gmail.com>
2023-10-25 03:07:18 +0300

    first commit

---
+++ src/main/java/com/korkmazyusufcan/weatherstudy/WeatherstudyApplication.java
@@ -0,0 +1,13 @@
+package com.korkmazyusufcan.weatherstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korkmazyusufcan/weatherstudy/WeatherstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korkmazyusufcan.weatherstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
